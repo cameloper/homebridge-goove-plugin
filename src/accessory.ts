abf2cdaa5571d5201eacc99930fae3815e18d629
Ihsan Batughan Yilmaz <cameloper@cameloper.com>
2021-01-22 20:01:24 +0100

    Added color options too

--- src/accessory.ts
+++ src/accessory.ts
@@ -104,6 +104,8 @@ class GoveeLedStrip implements AccessoryPlugin {
       })
       .on(CharacteristicEventTypes.SET, (value: CharacteristicValue, callback: CharacteristicSetCallback) => {
         log.info("Setting hue: " + value);
+        const color = this.hslToHex(value as number, this.saturation, this.brightness);
+        this.setRequest(GoveeCharacteristic.Color, color);
         this.hue = value as number;
         callback();
       });
@@ -114,6 +116,8 @@ class GoveeLedStrip implements AccessoryPlugin {
       })
       .on(CharacteristicEventTypes.SET, (value: CharacteristicValue, callback: CharacteristicSetCallback) => {
         log.info("Setting saturation: " + value);
+        const color = this.hslToHex(this.hue, value as number, this.brightness);
+        this.setRequest(GoveeCharacteristic.Color, color);
         this.saturation = value as number;
         callback();
       });
@@ -165,6 +169,17 @@ class GoveeLedStrip implements AccessoryPlugin {
     .catch(function (error: AxiosError) {
       log.error(error.message);
     });
+  }
+
+  hslToHex(h: number, s: number, l: number): string {
+    l /= 100;
+    const a = s * Math.min(l, 1 - l) / 100;
+    const f = (n: number) => {
+      const k = (n + h / 30) % 12;
+      const color = l - a * Math.max(Math.min(k - 3, 9 - k, 1), -1);
+      return Math.round(255 * color).toString(16).padStart(2, '0');   // convert to Hex and prefix "0" if needed
+    };
+    return `${f(0)}${f(8)}${f(4)}`;
   }
 }
 
